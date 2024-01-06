b32986cea24a46c4456c3f62373c49d9fc70bd6b
Ariel Sandoval <sandoval.aes@gmail.com>
2024-01-05 22:08:22 -0300

    incluye funciones de carrito y storagelocal

--- JS/app.js
+++ JS/app.js
@@ -1,145 +1,192 @@
-//DECLARACION DE CONSTANTES
-const SI = "si";
-const NO = "no";
-const SALIR = "salir";
-const ERROR = true;
+let productos = [];
+let planes = [];
+let carrito = {};
 
-//DECLARACION DE VARIABLES
-let opcionVersion;
-let opcionInstalacion;
-let nombreVersion; //Almacena el nombre de la versión seleccionada
-let nombreInstalacion; //Almacena el nombre del tipo de instalación seleccionada
+const items = document.getElementById('items');
+const footer = document.getElementById('footer');
+const templateFooter = document.getElementById('template-footer').content;
+const templateCarrito = document.getElementById('template-carrito').content;
+const fragment = document.createDocumentFragment();
 
-let cantidadUsuarios = 0; //Cantidad de usuarios ingresados
-let totalPresupuesto = 0; //Importe final del presupuesto
+document.addEventListener('DOMContentLoaded', e => { 
+                            obtenerProductos();
+                            obtenerPlanes();
+    
+                            if (localStorage.getItem("micarrito")){
+                                carrito = JSON.parse(localStorage.getItem("micarrito"));
+                                dibujarCarrito();
+                            }
+                        });
 
-let nombrecompleto; //Nombre ingresado correspondiente a la persona o empresa
-let existeError = false; //Flag que indica si hubo error durante el ingreso de datos
-let salir; //Flag booleano 
-let promocionSeleccionada; //Almacena el objeto de la clase "Plan" obtenido según los datos ingresados
+items.addEventListener('click', e => { btnAumentarDisminuir(e) });
 
-const promociones = []; //Array de objetos de la clase "Plan"
-let promosfiltradas; //Array de objetos de la clase "Plan" obtenidos luego de aplicar Filter, según la versión y el tipo de instalación
-
-debugger;
-
-//Inicialización de array de promociones o planes (objetos de tipo plan)
-promociones.push(new Plan('Nube', 'Basic', 1, 10, 1500));
-promociones.push(new Plan('Nube', 'Basic', 11, 20, 1250));
-promociones.push(new Plan('Nube', 'Basic', 21, 1000, 1000));
-promociones.push(new Plan('Local', 'Basic', 1, 10, 1300));
-promociones.push(new Plan('Local', 'Basic', 11, 20, 1100));
-promociones.push(new Plan('Local', 'Basic', 21, 1000, 900));
-
-promociones.push(new Plan('Nube', 'Standard', 1, 10, 1600));
-promociones.push(new Plan('Nube', 'Standard', 11, 20, 1350));
-promociones.push(new Plan('Nube', 'Standard', 21, 1000, 1100));
-promociones.push(new Plan('Local', 'Standard', 1, 10, 1400));
-promociones.push(new Plan('Local', 'Standard', 11, 20, 1200));
-promociones.push(new Plan('Local', 'Standard', 21, 1000, 1000));
-
-promociones.push(new Plan('Nube', 'Advanced', 1, 10, 1700));
-promociones.push(new Plan('Nube', 'Advanced', 11, 20, 1450));
-promociones.push(new Plan('Nube', 'Advanced', 21, 1000, 1200));
-promociones.push(new Plan('Local', 'Advanced', 1, 10, 1500));
-promociones.push(new Plan('Local', 'Advanced', 11, 20, 1300));
-promociones.push(new Plan('Local', 'Advanced', 21, 1000, 1100));
-
-
-do {
-        existeError = false;
-        nombrecompleto = prompt('Bienvenido a MoxiERP! \n Ingrese nombre y apellido del contacto. \n');
-        if (nombrecompleto === "") {
-                alert('Valor Incorrecto!');
-                existeError = true;
-        }
-} while (existeError && nombrecompleto !== null)
-
-if (nombrecompleto !== null) {
-        do {
-                do {
-                        existeError = false;
-                        opcionVersion = prompt(`Bienvenido/a ${nombrecompleto}` + '\n'
-                                + 'Ingrese el tipo de versión del sistema: \n'
-                                + '1 = Basic \n'
-                                + '2 = Standard \n'
-                                + '3 = Advanced \n');
-                        switch (opcionVersion) {
-                                case null: 
-                                        break;
-                                case '1': 
-                                        nombreVersion = 'Basic';
-                                        break;
-                                case '2': 
-                                        nombreVersion = 'Standard';
-                                        break;
-                                case '3': 
-                                        nombreVersion = 'Advanced';
-                                        break;
-                                default: alert('La versión ingresada es incorrecta.');
-                                        existeError = true;
-                                        break;
-                        }
-                } while (existeError && opcionVersion !== null)
-
-                if (opcionVersion !== null) {
-                        do {
-                                existeError = false;
-                                opcionInstalacion = prompt('Ingrese el tipo de instalación: \n'
-                                        + '1 = Local \n'
-                                        + '2 = En la Nube \n');
-                                switch (opcionInstalacion) {
-                                        case null:                              
-                                                break;
-                                        case '1': 
-                                                nombreInstalacion = 'Local';
-                                                break;
-                                        case '2':
-                                                nombreInstalacion = 'Nube';
-                                                break;
-                                        default: alert('El tipo de instalación ingresada es incorrecta.');
-                                                existeError = true;
-                                                break;
-                                }
-                        } while (existeError && opcionInstalacion !== null)
-                }
-
-                if (opcionVersion !== null && opcionInstalacion !== null) {
-                        do {
-                                cantidadUsuarios = parseInt(prompt('Ingrese el número de usuarios: '));
-                                if (Number.isInteger(cantidadUsuarios)) {
-                                        promosfiltradas = promociones.filter((item) => item.instalacion === nombreInstalacion && item.nombreversion === nombreVersion);
-                                        promocionSeleccionada = promosfiltradas.find((item) => cantidadUsuarios >= item.cant_usu_minimo && cantidadUsuarios <= item.cant_usu_maximo);
-                                        let importePlan = promocionSeleccionada.importe;
-                                        totalPresupuesto = importePlan * cantidadUsuarios;
-                                        existeError = false;
-                                }
-                                else {
-                                        existeError = true;
-                                        alert('Valor Incorrecto!');
-                                }
-                        } while (existeError && opcionVersion !== null)
-
-                        alert(`La cotización obtenida es la siguiente: \n Versión: ${nombreVersion}\n Tipo de Instalación: ${nombreInstalacion}\n Cantidad de Usuarios: ${cantidadUsuarios}\n Costo Licencia: $${promocionSeleccionada.importe} \n Importe Presupuesto: $${totalPresupuesto}\n`);
-
-                        do {
-                                existeError = false;
-                                salir = prompt('Desea realizar otra cotización? si/no');
-                                if (salir === "") {
-                                        alert('Valor Incorrecto!');
-                                        existeError = true;
-                                }
-                                if (salir !== null) {
-                                        salir = salir.toLowerCase();
-                                }
-                        } while (existeError && salir !== null)
-                }
-        } while (salir !== NO && salir !== null && opcionVersion !== null && opcionInstalacion !== null)
-        alert(`Muchas gracias ${nombrecompleto} por visitarnos.`);
+const agregarACarrito =  e => {
+    rellenarItemCarrito(e.target.parentElement);
 }
-else
-        alert(`Muchas gracias por visitarnos.`);
 
+const rellenarItemCarrito = objecto => {
+    const itemProducto = {
+        id: objecto.querySelector(".agregarProducto").dataset.id,
+        nombre: objecto.querySelector("h3").textContent,
+        precio: 1000,
+        cantidad: 1
+    }
+    if (carrito.hasOwnProperty(itemProducto.id)){
+        itemProducto.cantidad += 1;
+    }
 
+    carrito[itemProducto.id] = {...itemProducto};
+    dibujarCarrito();
+}
 
+const dibujarCarrito = () => {
+    items.innerHTML = ''
 
+    Object.values(carrito).forEach(producto => {
+        templateCarrito.querySelector('th').textContent = producto.id
+        templateCarrito.querySelectorAll('td')[0].textContent = producto.nombre
+        templateCarrito.querySelectorAll('td')[1].textContent = producto.cantidad
+        templateCarrito.querySelector('span').textContent = producto.precio * producto.cantidad
+        
+        //botones
+        templateCarrito.querySelector('.btn-info').dataset.id = producto.id
+        templateCarrito.querySelector('.btn-danger').dataset.id = producto.id
+
+        const clone = templateCarrito.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    items.appendChild(fragment);
+
+    dibujarFooter();
+    localStorage.setItem("micarrito",JSON.stringify(carrito));
+}
+
+const dibujarFooter = () => {
+    footer.innerHTML = ''
+    
+    if (Object.keys(carrito).length === 0) {
+        footer.innerHTML = `
+        <th scope="row" colspan="5">Carrito vacío con innerHTML</th>
+        `
+        Toastify(
+            {
+                text:'Carrito Vacio',
+                duraction: 3000,
+                close: true,
+                gravity: 'top',
+                position: 'right',
+                stopOnFocus: true,
+                offset: {
+                    x: 0,
+                    y: 64 
+                },
+                style: {
+                    background: "linear-gradient(to left, #d49435, #EC7063)",
+                }
+            }
+        ).showToast();
+        return
+    }
+    
+    // sumar cantidad y sumar totales
+    const nCantidad = Object.values(carrito).reduce((acc, { cantidad }) => acc + cantidad, 0);
+    const nPrecio = Object.values(carrito).reduce((acc, {cantidad, precio}) => acc + cantidad * precio ,0);
+
+    templateFooter.querySelectorAll('td')[0].textContent = nCantidad;
+    templateFooter.querySelector('span').textContent = nPrecio;
+
+    const clone = templateFooter.cloneNode(true);
+    fragment.appendChild(clone);
+
+    footer.appendChild(fragment);
+
+    const boton = document.querySelector('#vaciar-carrito')
+    boton.addEventListener('click', () => {
+        carrito = {};
+        dibujarCarrito();
+    })
+}
+
+const btnAumentarDisminuir = e => {
+
+    if (e.target.classList.contains('btn-info')) {
+        const producto = carrito[e.target.dataset.id];
+        producto.cantidad++;
+        carrito[e.target.dataset.id] = { ...producto };
+        dibujarCarrito();
+    }
+
+    if (e.target.classList.contains('btn-danger')) {
+        const producto = carrito[e.target.dataset.id]
+        producto.cantidad--;
+        if (producto.cantidad === 0) {
+            delete carrito[e.target.dataset.id];
+        } else {
+            carrito[e.target.dataset.id] = {...producto};
+        }
+        dibujarCarrito();
+    }
+    e.stopPropagation()
+}
+
+async function dibujarTarjetas(pproductos)
+{
+    const contenedor = document.querySelector(".flex-contenedor-tarjetas");
+    pproductos.forEach(element => {
+        const item = document.createElement("div");
+        item.innerHTML = `
+        <h3>${element.nombre}</h3>
+        <p>${element.descripcion} </p>
+        <button class="agregarProducto" id="${element.nombre}" data-id="${element.id}"  >Agregar</button>
+        `
+        item.classList.add("flex-tarjeta");
+        contenedor.append(item)
+    });
+}
+
+async function obtenerProductos()
+{
+    const response =  await fetch("../productos.json");
+    if (response.ok){
+        productos = await response.json();
+        await dibujarTarjetas(productos);
+        console.log(productos);
+        const botonesAgregar = document.querySelectorAll(".agregarProducto");
+        inicilizarEventoClickBotonAddCarrito(botonesAgregar);
+
+    }
+}
+
+async function obtenerPlanes()
+{
+    const response =  await fetch("../planes.json");
+    if (response.ok){
+        planes = await response.json();
+    }
+}
+
+function inicilizarEventoClickBotonAddCarrito(botonesAgregar){
+    for (var i=0; i<botonesAgregar.length; i++) {
+            
+        botonesAgregar[i].addEventListener('click', e =>{
+            agregarACarrito(e);
+            Toastify(
+                    {
+                        text:'Item Agregado al Carrito',
+                        duraction: 3000,
+                        close: true,
+                        gravity: 'top',
+                        position: 'right',
+                        stopOnFocus: true,
+                        offset: {
+                            x: 0,
+                            y: 64 
+                        },
+                        style: {
+                            background: "linear-gradient(to left, #2a4151f4, #2a4151f4)",
+                        }
+                    }
+            ).showToast();
+        })
+    }
+}
